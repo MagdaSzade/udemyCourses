9f1ce7a6efd501550b203b438f19038d200441bb
MagdaSzade <magdalenasuchanek@gmail.com>
2020-06-16 21:48:51 +0200

    Communicating with Props

--- Modern React with Redux/components/src/index.js
+++ Modern React with Redux/components/src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
     return (
         <div className="ui cointainer comments">
-            <CommentDetail 
-                author="Sam"  
-                imgSrc={faker.image.avatar()} 
-                time="Today 6PM" text="Nice blog"
-            />
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam"  
+                    imgSrc={faker.image.avatar()} 
+                    time="Today 6PM" text="Nice blog"
+                />
+            </ApprovalCard>
         </div>
     )
 };
